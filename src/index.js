59906fd88163cd93412fe24ead9fdde3f51cb5d7
unknown <rahataminulislam37@gmail.com>
2022-11-29 18:21:45 +0600

    api call

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from './Contexts/AuthProvider';
+import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Toaster></Toaster>
     </AuthProvider>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/ReviewSection/ReviewSection.js
+++ src/pages/ReviewSection/ReviewSection.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider';
 
@@ -7,14 +8,44 @@ const ReviewSection = () => {
     if (!user?.uid) {
         return <p className='my-5 text-xl'>Please <Link className='text-sky-400 text-3xl' to={'/login'}>LogIn</Link> to add review!!</p>
     }
+
+    const handleReviews = event => {
+        event.preventDefault();
+        const form = event.target;
+        const review = form.review.value;
+
+        const reviewData = {
+            review
+        }
+        console.log(review);
+
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(reviewData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    toast.success('Review Added Successfully')
+                    form.reset();
+
+                }
+            })
+            .catch(er => console.error(er));
+    }
+
     return (
         <div className='my-5'>
 
-            <form >
+            <form onSubmit={handleReviews}>
 
                 {user?.uid &&
                     <div className='justify-center'>
-                        <textarea className="textarea textarea-accent w-80 h-28" placeholder="Enter your Review"></textarea>
+                        <textarea name='review' className="textarea textarea-accent w-80 h-28" placeholder="Enter your Review"></textarea>
                         <input className='btn block btn-success text-white' type="submit" value="Add Review" />                </div>}
             </form>
 
