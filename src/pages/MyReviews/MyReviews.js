fac5e86e8f7321dd88d97214e056a0bd1e7350a8
unknown <rahataminulislam37@gmail.com>
2022-11-30 12:21:14 +0600

    my review added

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -42,33 +42,40 @@ const MyReviews = () => {
     return (
         <div>
             <h3 className="text-3xl mb-5">My Reviews</h3>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Title</th>
+            {
+                reviews.length < 1 ? <h2 className='text-3xl text-center my-28'>No Reviews Were Added !!</h2>
+                    :
+                    <div className="overflow-x-auto">
+                        <table className="table w-full">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Title</th>
 
-                            <th>Review</th>
-                            <th>Delete</th>
+                                    <th>Review</th>
+                                    <th>Edit Review</th>
+                                    <th>Delete</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            reviews.length > 0 &&
-                            reviews?.map((review, i) => <tr key={review._id}>
-                                <th>{i + 1}</th>
-                                <td>{review?.title}</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    reviews.length > 0 &&
+                                    reviews?.map((review, i) => <tr key={review._id}>
+                                        <th>{i + 1}</th>
+                                        <td>{review?.title}</td>
 
-                                <td>{review?.review}</td>
-                                <button onClick={() => handleDeleteReview(review)} className="btn btn-sm">Delete</button>
+                                        <td>{review?.review}</td>
+                                        <td><button className="btn btn-sm">Edit</button></td>
+                                        <td><button onClick={() => handleDeleteReview(review)} className="btn btn-sm btn-error ">Delete</button></td>
 
-                            </tr>)
-                        }
-                    </tbody>
-                </table>
-            </div>
+                                    </tr>)
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+            }
+
         </div>
     );
 };
